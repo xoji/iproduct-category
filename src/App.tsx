cecb8a01d3c5d2838bab802a43efcedbb53fb6ea
xoji <xoji3029@gmail.com>
2023-04-13 18:44:54 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+
+  const [state, setState] = useState({
+    text: "",
+  });
+
+  function onChange(e: React.FormEvent<HTMLInputElement>)  {
+    setState({ text: e.currentTarget.value });
+  }
+
+  function submit(e: React.FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    if (state) {
+      axios.post('https://iproduct.uz/api/category', {
+        name: state
+      }).then(res => {
+        if (res.data.success) {
+          (window as any).Telegram.WebApp.close();
+        }
+      });
+    }
+  }
+
+  return (
+    <div className={'form-container'}>
+      <input type="text" placeholder={'Имя категории'} className={'category-input'} onChange={onChange} value={state.text}/>
+      <button className={'submit-btn'} onClick={submit}>Добавить</button>
+    </div>
+  );
+}
+
+export default App;
